76f66cae4a6a4a6ee6694a3bb8d615222f57d358
Anthony K <lilaussie24@gmail.com>
2015-04-20 20:13:19 +1000

    Added content.

--- src/com/lilaussie/itemstoragesystem/StorageSystem.java
+++ src/com/lilaussie/itemstoragesystem/StorageSystem.java
@@ -1,8 +1,78 @@
 package com.lilaussie.itemstoragesystem;
 
+import com.lilaussie.itemstoragesystem.references.Reference;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Anthony on 19/04/2015.
  */
-public class StorageSystem {
+public class StorageSystem extends JFrame {
+
+    // Instance attributes used in this example
+    private	JPanel      leftPanel;
+    private	JTable		table;
+    private JTable      buttonTable;
+    private	JScrollPane scrollPane;
+    private JPanel      rightPanel;
+    private JTextArea   titleTextPane;
+
+    private int colCount;
+    private String columnNames[];
+    private	String dataValues[][];
+
+    public StorageSystem() {
+        setTitle(Reference.STRING_TITLE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(Reference.INT_WINDOW_WIDTH, Reference.INT_WINDOW_HEIGHT);
+        setBackground(Color.GRAY);
+
+        leftPanel = new JPanel();
+        leftPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        add(leftPanel);
+
+        CreateColumns();
+        CreateData();
+
+        table = new JTable(dataValues, Reference.STRING_COLUMN_NAMES);
+
+        table.setShowHorizontalLines(false);
+        table.setRowSelectionAllowed(true);
+        table.setColumnSelectionAllowed(true);
+
+        table.setSelectionForeground(Color.WHITE);
+        table.setSelectionBackground(Color.RED);
+
+        scrollPane = new JScrollPane(table);
+        scrollPane.setPreferredSize(new Dimension(Reference.INT_WINDOW_WIDTH / 2, Reference.INT_WINDOW_HEIGHT - 150));
+        leftPanel.add(scrollPane, BorderLayout.CENTER);
+
+        buttonTable = new JTable();
+        leftPanel.add(buttonTable);
+
+        titleTextPane = new JTextArea("title");
+        titleTextPane.setPreferredSize(new Dimension(150, 24));
+        titleTextPane.setAlignmentY(Component.TOP_ALIGNMENT);
+        leftPanel.add(titleTextPane);
+    }
+
+    public void CreateColumns() {
+        colCount = Reference.STRING_COLUMN_NAMES.length;
+    }
+
+    public void CreateData() {
+        dataValues = new String[100][colCount];
+        for (int iY = 0; iY < 100; iY++) {
+            for (int iX = 0; iX < colCount; iX++) {
+                dataValues[iY][iX] = "" + iX + "," + iY;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        StorageSystem mainFrame = new StorageSystem();
+        mainFrame.setVisible(true);
+    }
 
 }
---
+++ src/com/lilaussie/itemstoragesystem/references/Reference.java
@@ -0,0 +1,15 @@
+package com.lilaussie.itemstoragesystem.references;
+
+/**
+ * Created by Anthony K on 4/20/2015.
+ */
+public class Reference {
+
+    public static String STRING_TITLE = "Item Storage System";
+
+    public static int INT_WINDOW_WIDTH = 1280;
+    public static int INT_WINDOW_HEIGHT  = 720;
+
+    public static String[] STRING_COLUMN_NAMES =  { "Title", "Location" };
+
+}
